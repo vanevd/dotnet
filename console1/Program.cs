b2decbad220a373a491e60d63fb5120671ff6e8d
Dimitar Vanev <radibase@gmail.com>
2017-12-10 10:02:02 +0200

    gitignore

--- console1/Program.cs
+++ console1/Program.cs
@@ -15,7 +15,7 @@ namespace console1
             width = 10.256;
             height = 20.789;
 
-            month = 6;
+            month = 7;
 
             Console.WriteLine("height = {1:n}, width = {0:f2}, month = {2:d2}", width, height, month);
             
