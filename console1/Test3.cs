3846650d70dd6709b2963696f82173cfd7b16dd4
Dimitar Vanev <radibase@gmail.com>
2017-12-17 11:29:35 +0200

    test3

--- console1/Test3.cs
+++ console1/Test3.cs
@@ -8,10 +8,14 @@ namespace Console1
         {
             string line;
             string[] list;
+            string[] list1;
+            int init_size = 5;
             int count=0;
             int i;
+            int size;
+            int key;
 
-            list = new string[10];
+            list = new string[init_size];
             Console.WriteLine("Test 3");
             while(true) {
                 Console.WriteLine("Please enter a command:");
@@ -21,13 +25,21 @@ namespace Console1
                 }
                 if (line.ToLower() == "list") {
                     for(i=0; i<count; i++) {                
-                        Console.WriteLine(list[i]);
+                        Console.WriteLine("{0}. {1}", i+1, list[i]);
                     }
+                    continue;
+                }
+                if (line.ToLower() == "count") {
+                    Console.WriteLine("Count: {0}", count);
+                    continue;
+                }
+                if (line.ToLower() == "size") {
+                    Console.WriteLine("Size: {0}", init_size);
                     continue;
                 }
                 if (line.Length > 4) {
                     if (line.Substring(0,4).ToLower() == "add ") {
-                        if (count<10) {
+                        if (count<init_size) {
                         list[count] = line.Substring(4,line.Length-4);
                         count++;
                         } else {
@@ -35,6 +47,32 @@ namespace Console1
                         }
                         continue;
                     }
+                    if (line.Substring(0,4).ToLower() == "del ") {
+                        key = Int32.Parse(line.Substring(4,line.Length-4));
+                        if ((key < 1)||(key > count)) {
+                            Console.WriteLine("Invalid key.");
+                            continue;
+                        }
+                        for (i=key-1; i<=count-2; i++) {
+                            list[i] = list[i+1];
+                        }
+                        count--;
+                        continue;
+                    }    
+                    if (line.Substring(0,5).ToLower() == "size ") {
+                        size = Int32.Parse(line.Substring(5,line.Length-5));
+                        if (size <= count) {
+                            Console.WriteLine("Please enter greater size.");
+                            continue;
+                        }
+                        list1 = new string[size];
+                        for(i=0; i<count; i++) {                
+                            list1[i] = list[i];
+                        }                      
+                        list = list1;  
+                        init_size = size;
+                        continue;
+                    }    
                 }
                 Console.WriteLine("Unknown Command");
             }
